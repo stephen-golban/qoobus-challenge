c41ba55396fd46f60e4255e6a815f4bc35f113fd
Stefan Golban <escgamingt.o@gmail.com>
2021-09-16 02:49:44 +0300

    feat: added basic framer animations to the graphs

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,19 +1,27 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import styles from './homepage.module.css'
 import { ColumnChart, LineChart } from '../components/common'
 
 const Homepage: React.FC = () => {
+  const chartArr = [
+    { component: ColumnChart, title: 'Ant Design Chart' },
+    { component: LineChart, title: 'React Chartjs 2 Chart (Using a dummy api)' },
+  ]
   return (
     <div className={styles.homepage}>
       <div className={styles.chartWrapper}>
-        <div className={styles.chartBox}>
-          <div className={styles.chartTitle}>React Chartjs 2 Chart (Using a dummy api)</div>
-          <LineChart />
-        </div>
-        <div className={styles.chartBox}>
-          <div className={styles.chartTitle}>Ant Design Chart</div>
-          <ColumnChart />
-        </div>
+        {chartArr.map((chart, i) => (
+          <motion.div
+            key={i}
+            className={styles.chartBox}
+            whileHover={{ y: -30, transition: { duration: 0.5 } }}
+            whileTap={{ scale: 2, transition: { duration: 0.5 } }}
+          >
+            <div className={styles.chartTitle}>{chart.title}</div>
+            <chart.component />
+          </motion.div>
+        ))}
       </div>
     </div>
   )
